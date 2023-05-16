03af15d1517eadcfbcdf2dc2c2703578e4111c65
Amandalfs <caduc57@gmail.com>
2023-05-16 12:26:35 -0300

    settigs
    importacao personalizada

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,17 @@
-import 'dotenv/config'
-import { z } from "zod"
+import "dotenv/config";
+import { z } from "zod";
 
 const schemaEnv = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-    PORT: z.coerce.number().default(3333)
-})
+	NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+	PORT: z.coerce.number().default(3333)
+});
 
-const _env = schemaEnv.safeParse(process.env)
+const _env = schemaEnv.safeParse(process.env);
 
 if(_env.success === false){
-    console.error('❌Invalid Enviroment variable', _env.error)
+	console.error("❌Invalid Enviroment variable", _env.error);
 
-    throw new Error('Invalid Enviroment variable')
+	throw new Error("Invalid Enviroment variable");
 }
 
-export const env = _env.data
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app";
-import { env } from "./env";
+import { env } from "./env/index";
 
 app.listen({
-    host: '0.0.0.0',
-    port: env.PORT
+	host: "0.0.0.0",
+	port: env.PORT
 }).then(()=>{
-    console.log("🚀 HTTP server running!")
-})
+	console.log("🚀 HTTP server running!");
+});
