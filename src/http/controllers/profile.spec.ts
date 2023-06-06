9291b72a7122d195f4a472bd6ecf8e6c06df5e30
Amandalfs <amandalfs57@gmail.com>
2023-06-06 10:24:56 -0300

    test-e2e: testado o profile do usuario junto com o banco  e a autenticacao

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Profile (e2)", async ()=>{
+	beforeAll(async()=>{
+		await app.ready();
+	});
+
+	afterAll(async()=>{
+		await app.close();
+	});
+
+	it("should be able to profile", async ()=>{
+		await request(app.server)
+			.post("/users")
+			.send({
+				name:"John Doe",
+				email:"johndoe@gmail.com",
+				password: "123456",
+			});	
+		const { body } = await request(app.server)
+			.post("/users/sessions")
+			.send({
+				email:"johndoe@gmail.com",
+				password: "123456",
+			});
+        
+		const response = await request(app.server)
+			.get("/me")
+			.set("Authorization", `Bearer ${body.token}`);
+
+		expect(response.statusCode).toEqual(200);
+		expect(response.body.user).toEqual(expect.any(Object));
+		expect(response.body.user).toEqual(expect.objectContaining({
+			email: "johndoe@gmail.com",
+		}));
+	});
+});
