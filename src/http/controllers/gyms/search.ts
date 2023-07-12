17cbf7bef1df4ee474b8ad50df139e7d19e7c56d
Amandalfs <amandalfs57@gmail.com>
2023-07-12 13:55:31 -0300

    feat: criado o controller de procurar  academia

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeSearchGymsUseCase } from "@/use-cases/factores/make-search-gyms-useCase";
+
+
+export async function search(request: FastifyRequest, reply: FastifyReply){
+	const searchQuertSchema = z.object({
+		q: z.string(),
+        page: z.coerce.number().min(1).default(1),
+	});
+
+	const { q, page } = searchQuertSchema.parse(request.body);
+
+	const searchGymsUseCase = makeSearchGymsUseCase();
+
+	const { gyms } = await searchGymsUseCase.execute( { query: q, page});
+
+
+	return reply.status(201).send({gyms});
+}
