34d544e0a84b788841657de4e50f07fcae701177
Amandalfs <amandalfs57@gmail.com>
2023-09-25 16:52:42 -0300

    test: create test e2e by validate check in

---
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -0,0 +1,48 @@
+import { createAndAuthenticatedUser } from "@/utils/test/create-and-authenticated-user";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
+
+describe("Nearby gym e2e tests", ()=>{
+	beforeAll(async()=>{
+		await app.ready();
+	});
+
+	afterAll(async()=>{
+		await app.close();
+	});
+
+	it("Should validate check-in", async ()=>{
+		const token = await createAndAuthenticatedUser(app);
+		await request(app.server)
+			.post("/gyms")
+			.set("Authorization", `Bearer ${token}`)
+			.send({
+				title: "Near Gym",
+				description: "JavaScript Gym description",
+				phone: "9181225187",
+				latitude: -23.2722115,
+				longitude: -50.8140629,
+			});
+
+		const { body: bodyGyms } = await request(app.server)
+			.get("/gyms/nearby?latitude=-23.2722115&longitude=-50.8140629")
+			.set("Authorization", `Bearer ${token}`);
+
+		await request(app.server)
+			.post(`/gyms/${bodyGyms.gyms[0].id}/check-ins`)
+			.set("Authorization", `Bearer ${token}`)
+			.send({
+				latitude: -23.2722115,
+				longitude: -50.8140629,
+			});
+		
+		const { body:bodyCheckins } = await request(app.server)
+			.get("/check-ins/history?page=1")
+			.set("Authorization", `Bearer ${token}`);
+		const { status } = await request(app.server)
+			.patch(`/check-ins/${bodyCheckins.checkIns[0].id}/validate`)
+			.set("Authorization", `Bearer ${token}`);
+		expect(status).toEqual(204);
+	});
+});
